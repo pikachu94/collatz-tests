15b1f945bf7f2d126227c5063858937c221784eb
ayenabil <ahmedalnabil@utexas.edu>
2017-02-02 17:11:21 -0600

    Added files for aa72359

---
+++ aa72359-TestCollatz.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+# -------------------------------
+# projects/collatz/TestCollatz.py
+# Copyright (C) 2016
+# Glenn P. Downing
+# -------------------------------
+
+# https://docs.python.org/3.4/reference/simple_stmts.html#grammar-token-assert_stmt
+
+# -------
+# imports
+# -------
+
+from io import StringIO
+from unittest import main, TestCase
+
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve, compute_all
+
+# -----------
+# TestCollatz
+# -----------
+
+
+class TestCollatz (TestCase):
+    # ----
+    # read
+    # ----
+
+    def test_read(self):
+        s = "1 10\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  1)
+        self.assertEqual(j, 10)
+
+    def test_read_2(self):
+        s = "100 200\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 100)
+        self.assertEqual(j, 200)
+
+    def test_read_3(self):
+        s = "201 210\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 201)
+        self.assertEqual(j, 210)
+
+    def test_read_4(self):
+        s = "900 1000\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 900)
+        self.assertEqual(j, 1000)
+
+    # ----
+    # eval
+    # ----
+
+    def test_eval_1(self):
+        v = collatz_eval(1, 10)
+        self.assertEqual(v, 20)
+
+    def test_eval_2(self):
+        v = collatz_eval(100, 200)
+        self.assertEqual(v, 125)
+
+    def test_eval_3(self):
+        v = collatz_eval(201, 210)
+        self.assertEqual(v, 89)
+
+    def test_eval_4(self):
+        v = collatz_eval(900, 1000)
+        self.assertEqual(v, 174)
+
+    #compute_all
+
+    def test_compute_all_1(self):
+        v = compute_all(2400, 500)
+        self.assertEqual(v, 182)
+
+    def test_compute_all_2(self):
+        v = compute_all(1200, 3700)
+        self.assertEqual(v, 217)
+
+    def test_compute_all_3(self):
+        v = compute_all(1, 200)
+        self.assertEqual(v, 125)
+
+    # -----
+    # print
+    # -----
+
+    def test_print(self):
+        w = StringIO()
+        collatz_print(w, 1, 10, 20)
+        self.assertEqual(w.getvalue(), "1 10 20\n")
+
+    def test_print_2(self):
+        w = StringIO()
+        collatz_print(w, 100, 200, 125)
+        self.assertEqual(w.getvalue(), "100 200 125\n")
+
+    def test_print_3(self):
+        w = StringIO()
+        collatz_print(w, 201, 210, 89)
+        self.assertEqual(w.getvalue(), "201 210 89\n")
+
+    def test_print_4(self):
+        w = StringIO()
+        collatz_print(w, 900, 1000, 174)
+        self.assertEqual(w.getvalue(), "900 1000 174\n")
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve(self):
+        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2(self):
+        r = StringIO("30 40\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "30 40 107\n")
+
+    def test_solve_3(self):
+        r = StringIO("64 20\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "64 20 113\n")
+
+    def test_solve_4(self):
+        r = StringIO("170 400\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "170 400 144\n")
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    main()
+
+""" #pragma: no cover
+% coverage-3.5 run --branch TestCollatz.py >  TestCollatz.out 2>&1
+
+% cat TestCollatz.out
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.000s
+
+OK
+
+% coverage-3.5 report -m                   >> TestCollatz.out
+
+% cat TestCollatz.out
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.000s
+
+OK
+Name             Stmts   Miss Branch BrPart  Cover   Missing
+------------------------------------------------------------
+Collatz.py          12      0      2      0   100%
+TestCollatz.py      32      0      0      0   100%
+------------------------------------------------------------
+TOTAL               44      0      2      0   100%
+"""
